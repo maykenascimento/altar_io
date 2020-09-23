32ff3b864f82c47afb1c09a441d74fff84a73da1
Mayke Nascimento <mayke.eng@outlook.com>
2020-09-23 23:45:45 +0100

    [FEAT] Added routes to Generator and Payments components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GeneratorComponent } from './components/generator/generator.component';
+import { PaymentsComponent } from './components/payments/payments.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "/generator", component: GeneratorComponent },
+  { path: "/payments", component: PaymentsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
